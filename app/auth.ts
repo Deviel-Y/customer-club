8198162f2b722bd492223461b9342fdb22ba8562
Daniel Yadghar <daniel072.dy@gmail.com>
2024-09-13 18:39:25 +0330

    Add credentials provider

--- app/auth.ts
+++ app/auth.ts
@@ -1,5 +1,75 @@
+import prisma from "@/prisma/client";
+import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: {
+          name: "email",
+          label: "Email",
+          type: "email",
+          placeholder: "example@domail.com",
+        },
+        password: {
+          name: "password",
+          label: "Password",
+          type: "password",
+          placeholder: "Account Password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials.email || !credentials.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (!user) return null;
+
+        const isPasswordsMatch = await bcrypt.compare(
+          credentials.password as string,
+          user.hashedPassword!
+        );
+
+        return isPasswordsMatch ? user : null;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user, session, account, trigger }) {
+      if (trigger === "update") {
+        token.name = session.name;
+        token.email = session.email;
+        token.picture = session.image;
+      }
+
+      if (user) {
+        return {
+          ...token,
+          id: user.id,
+          name: token.name,
+          email: token.email,
+          image: token.picture,
+        };
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.sub,
+          provider: token.provider,
+        },
+      };
+    },
+  },
 });
