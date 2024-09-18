012055f520089485ee016343462c0f753185289c
Daniel Yadghar <daniel072.dy@gmail.com>
2024-09-18 18:09:54 +0330

    Create authorizeUser function

---
+++ app/utils/authorizeRole.ts
@@ -0,0 +1,13 @@
+import { Session } from "next-auth";
+import { redirect } from "next/navigation";
+
+export const authorizeUser = (session: Session) => {
+  if (!session) redirect("/api/auth/signin");
+
+  if (session.user.role !== "USER") redirect("/admin");
+};
+export const authorizeAdmin = (session: Session) => {
+  if (!session) redirect("/api/auth/signin");
+
+  if (session.user.role !== "ADMIN") redirect("/");
+};
