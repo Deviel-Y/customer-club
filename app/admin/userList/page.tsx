e61048073387cb6eadfee90f9bf6477334efaf24
Daniel Yadghar <daniel072.dy@gmail.com>
2024-09-24 11:47:35 +0330

    Be able to search for specific user

---
+++ app/admin/userList/page.tsx
@@ -0,0 +1,38 @@
+import prisma from "@/prisma/client";
+import { User } from "@prisma/client";
+import UserListTable from "./UserListTable";
+import UserSearchField from "./UserSearchField";
+
+interface Props {
+  searchParams: {
+    companyName: string;
+    companyBranch: string;
+    email: string;
+    itManager: string;
+    pageNumber: string;
+  };
+}
+
+const UserListPage = async ({
+  searchParams: { companyBranch, companyName, email, itManager, pageNumber },
+}: Props) => {
+  const users: User[] = await prisma.user.findMany({
+    where: {
+      role: "USER",
+      companyName: { contains: companyName },
+      companyBranch: { contains: companyBranch },
+      email: { contains: email },
+      itManager: { contains: itManager },
+    },
+  });
+
+  return (
+    <div className="p-5 flex flex-col">
+      <UserSearchField />
+
+      <UserListTable users={users} />
+    </div>
+  );
+};
+
+export default UserListPage;
