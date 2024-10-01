c54b18dc5533294dbc51e67309a8746aba605194
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-01 18:08:12 +0330

    UI improvment : show 6 users in table & align search inputs in 1 line

--- app/admin/userList/UserSearchField.tsx
+++ app/admin/userList/UserSearchField.tsx
@@ -2,146 +2,145 @@
 
 import { Button, Input } from "@nextui-org/react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { ChangeEvent } from "react";
 
 const UserSearchField = () => {
   const searchParmas = useSearchParams();
   const router = useRouter();
 
+  const componyNameOnChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const newParams = new URLSearchParams(searchParmas);
+
+    if (event.target.value) {
+      newParams.set("companyName", event.target.value as string);
+    } else {
+      newParams.delete("companyName");
+    }
+
+    if (searchParmas.get("companyBranch"))
+      newParams.set("companyBranch", searchParmas.get("companyBranch")!);
+
+    if (searchParmas.get("itManager"))
+      newParams.set("itManager", searchParmas.get("itManager")!);
+
+    if (searchParmas.get("email"))
+      newParams.set("email", searchParmas.get("email")!);
+
+    if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
+
+    router.push(`?${newParams.toString()}`);
+  };
+
+  const componyBranchOnChangeHandler = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    const newParams = new URLSearchParams(searchParmas);
+
+    if (event.target.value) {
+      newParams.set("companyBranch", event.target.value as string);
+    } else {
+      newParams.delete("companyBranch");
+    }
+
+    if (searchParmas.get("companyName"))
+      newParams.set("companyName", searchParmas.get("companyName")!);
+
+    if (searchParmas.get("itManager"))
+      newParams.set("itManager", searchParmas.get("itManager")!);
+
+    if (searchParmas.get("email"))
+      newParams.set("email", searchParmas.get("email")!);
+
+    if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
+
+    router.push(`?${newParams.toString()}`);
+  };
+
+  const emailAddressOnChangeHandler = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    const newParams = new URLSearchParams(searchParmas);
+
+    if (event.target.value) {
+      newParams.set("email", event.target.value as string);
+    } else {
+      newParams.delete("email");
+    }
+
+    if (searchParmas.get("companyBranch"))
+      newParams.set("companyBranch", searchParmas.get("companyBranch")!);
+
+    if (searchParmas.get("itManager"))
+      newParams.set("itManager", searchParmas.get("itManager")!);
+
+    if (searchParmas.get("companyName"))
+      newParams.set("companyName", searchParmas.get("companyName")!);
+
+    if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
+
+    router.push(`?${newParams.toString()}`);
+  };
+
+  const itMnagerOnChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const newParams = new URLSearchParams(searchParmas);
+
+    if (event.target.value) {
+      newParams.set("itManager", event.target.value as string);
+    } else {
+      newParams.delete("itManager");
+    }
+
+    if (searchParmas.get("companyBranch"))
+      newParams.set("companyBranch", searchParmas.get("companyBranch")!);
+
+    if (searchParmas.get("email"))
+      newParams.set("email", searchParmas.get("email")!);
+
+    if (searchParmas.get("companyName"))
+      newParams.set("companyName", searchParmas.get("companyName")!);
+
+    if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
+
+    router.push(`?${newParams.toString()}`);
+  };
+
   return (
-    <div className=" flex flex-col gap-5">
+    <div className=" flex flex-row gap-5">
       <Button
         onPress={() => router.push("/admin/createNewUser")}
-        size="lg"
-        className="self-start"
+        className="self-center"
         color="secondary"
         variant="shadow"
       >
         تعریف کاربر جدید
       </Button>
 
-      <div className="grid grid-cols-4 gap-5 mb-5">
+      <div className="grid grid-cols-4 grid-rows-1 gap-5 mb-5 w-full">
         <Input
-          onChange={(event) => {
-            const newParams = new URLSearchParams(searchParmas);
-
-            if (event.target.value) {
-              newParams.set("companyName", event.target.value as string);
-            } else {
-              newParams.delete("companyName");
-            }
-
-            if (searchParmas.get("companyBranch"))
-              newParams.set(
-                "companyBranch",
-                searchParmas.get("companyBranch")!
-              );
-
-            if (searchParmas.get("itManager"))
-              newParams.set("itManager", searchParmas.get("itManager")!);
-
-            if (searchParmas.get("email"))
-              newParams.set("email", searchParmas.get("email")!);
-
-            if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
-
-            router.push(`?${newParams.toString()}`);
-          }}
+          onChange={componyNameOnChangeHandler}
           label="نام سازمان"
           type="search"
-          size="lg"
           variant="underlined"
         />
 
         <Input
-          onChange={(event) => {
-            const newParams = new URLSearchParams(searchParmas);
-
-            if (event.target.value) {
-              newParams.set("companyBranch", event.target.value as string);
-            } else {
-              newParams.delete("companyBranch");
-            }
-
-            if (searchParmas.get("companyName"))
-              newParams.set("companyName", searchParmas.get("companyName")!);
-
-            if (searchParmas.get("itManager"))
-              newParams.set("itManager", searchParmas.get("itManager")!);
-
-            if (searchParmas.get("email"))
-              newParams.set("email", searchParmas.get("email")!);
-
-            if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
-
-            router.push(`?${newParams.toString()}`);
-          }}
+          onChange={componyBranchOnChangeHandler}
           label="شعبه"
           type="search"
-          size="lg"
           variant="underlined"
         />
 
         <Input
-          onChange={(event) => {
-            const newParams = new URLSearchParams(searchParmas);
-
-            if (event.target.value) {
-              newParams.set("email", event.target.value as string);
-            } else {
-              newParams.delete("email");
-            }
-
-            if (searchParmas.get("companyBranch"))
-              newParams.set(
-                "companyBranch",
-                searchParmas.get("companyBranch")!
-              );
-
-            if (searchParmas.get("itManager"))
-              newParams.set("itManager", searchParmas.get("itManager")!);
-
-            if (searchParmas.get("companyName"))
-              newParams.set("companyName", searchParmas.get("companyName")!);
-
-            if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
-
-            router.push(`?${newParams.toString()}`);
-          }}
+          onChange={emailAddressOnChangeHandler}
           label="آدرس ایمیل"
           type="search"
-          size="lg"
           variant="underlined"
         />
 
         <Input
-          onChange={(event) => {
-            const newParams = new URLSearchParams(searchParmas);
-
-            if (event.target.value) {
-              newParams.set("itManager", event.target.value as string);
-            } else {
-              newParams.delete("itManager");
-            }
-
-            if (searchParmas.get("companyBranch"))
-              newParams.set(
-                "companyBranch",
-                searchParmas.get("companyBranch")!
-              );
-
-            if (searchParmas.get("email"))
-              newParams.set("email", searchParmas.get("email")!);
-
-            if (searchParmas.get("companyName"))
-              newParams.set("companyName", searchParmas.get("companyName")!);
-
-            if (searchParmas.get("pageNumber")) newParams.delete("pageNumber");
-
-            router.push(`?${newParams.toString()}`);
-          }}
+          onChange={itMnagerOnChangeHandler}
           label="مسئول انفوماتیک"
           type="search"
-          size="lg"
           variant="underlined"
         />
       </div>
