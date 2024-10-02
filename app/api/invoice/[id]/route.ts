c22d22d86db316ab2d99ac6e1d845e7e1b52e75a
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-02 15:36:45 +0330

    Build PATCH API to update invoice

--- app/api/invoice/[id]/route.ts
+++ app/api/invoice/[id]/route.ts
@@ -1,3 +1,5 @@
+import getSession from "@/app/libs/getSession";
+import { invoiceSchema, InvoiceSchemaType } from "@/app/libs/validationSchema";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -14,4 +16,53 @@ export const DELETE = async (
   });
 
   return NextResponse.json(deletedInvoice);
+};
+
+export const PATCH = async (
+  request: NextRequest,
+  { params: { id } }: Props
+) => {
+  try {
+    const session = await getSession();
+
+    const body: InvoiceSchemaType = await request.json();
+    const { assignedToUserId, description, invoiceNumber, organizationBranch } =
+      body;
+
+    const organization = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    const invoice = await prisma.invoice.findUnique({ where: { id } });
+    if (!invoice)
+      return NextResponse.json("Invoice not found", { status: 404 });
+
+    const validation = invoiceSchema.safeParse(body);
+    if (!validation.success)
+      return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const similerInvoice = await prisma.invoice.findFirst({
+      where: { invoiceNumber: invoiceNumber.trim() },
+    });
+    if (similerInvoice)
+      return NextResponse.json("فاکتور با این شماره فاکتور موجود میباشد.", {
+        status: 400,
+      });
+
+    const updatedInvoice = await prisma.invoice.update({
+      where: { id },
+      data: {
+        assignedToUserId,
+        description,
+        invoiceNumber,
+        organizationBranch,
+        issuerId: session?.user.id,
+        organization: organization?.companyName as string,
+      },
+    });
+
+    return NextResponse.json(updatedInvoice);
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
 };
