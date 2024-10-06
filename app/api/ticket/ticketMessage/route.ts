6c2541d0d7bdf906f263e80d694b2e730d1bb83f
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-06 20:12:58 +0330

    Build a POST API to create a ticketMessage

---
+++ app/api/ticket/ticketMessage/route.ts
@@ -0,0 +1,23 @@
+import { ticketMessageSchema } from "@/app/libs/validationSchema";
+import prisma from "@/prisma/client";
+import { TicketMessage } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const body: TicketMessage = await request.json();
+    const { assignetoTicketId, message } = body;
+
+    const validation = ticketMessageSchema.safeParse(body);
+    if (!validation.success)
+      return NextResponse.json(validation.error.format(), { status: 400 });
+
+    const newMesaage = await prisma.ticketMessage.create({
+      data: { message, assignetoTicketId },
+    });
+
+    return NextResponse.json(newMesaage);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+};
