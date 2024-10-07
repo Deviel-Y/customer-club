714c8f919830a3d4761f574de1eb93937dc101c9
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-07 20:15:47 +0330

    Create AdminTicketListPage

--- app/admin/ticket/page.tsx
+++ app/admin/ticket/page.tsx
@@ -1,5 +1,21 @@
-const AdminTicketListPage = () => {
-  return <div>AdminTicketListPage</div>;
+import getSession from "@/app/libs/getSession";
+import { authorizeAdmin } from "@/app/utils/authorizeRole";
+import prisma from "@/prisma/client";
+import TicketListTable from "./TicketListTable";
+
+const AdminTicketListPage = async () => {
+  const session = await getSession();
+  authorizeAdmin(session!);
+
+  const tickets = await prisma.ticket.findMany({
+    include: { User: true },
+  });
+
+  return (
+    <div className="px-5 py-2 w-full">
+      <TicketListTable tickets={tickets} />
+    </div>
+  );
 };
 
 export default AdminTicketListPage;
