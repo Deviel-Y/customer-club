d8705402a1c6a37b16f57c98597109ef2019c651
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-11 19:38:34 +0330

    Build invoiceSummery component

---
+++ app/admin/invoice-issuing/InvoiceSummery.tsx
@@ -0,0 +1,40 @@
+import formatNumber from "@/app/utils/formatNumber";
+import { Invoice } from "@prisma/client";
+
+interface Props {
+  invoices: Invoice[];
+}
+
+const InvoiceSummery = ({ invoices }: Props) => {
+  return (
+    <div className="flex flex-row justify-start gap-20 mt-5">
+      <div className="flex flex-row justify-center items-center">
+        <p className="text-base text-gray-500 me-2">جمع مبالغ:</p>
+        <p className="text-[25px]">
+          {formatNumber(invoices?.reduce((acc, sum) => acc + sum.price, 0))}
+        </p>
+        <p className="text-base text-gray-600 ms-1">ريال</p>
+      </div>
+
+      <div className="flex flex-row justify-center items-center">
+        <p className="text-base text-gray-500 me-2">جمع مالیات:</p>
+        <p className="text-[25px]">
+          {formatNumber(invoices?.reduce((acc, sum) => acc + sum.tax, 0))}
+        </p>
+        <p className="text-base text-gray-600 ms-1">ريال</p>
+      </div>
+
+      <div className="flex flex-row justify-center items-center">
+        <p className="text-base text-gray-500 me-2">جمع مبالغ نهایی:</p>
+        <p className="text-[25px]">
+          {formatNumber(
+            invoices?.reduce((acc, sum) => acc + sum.priceWithTax, 0)
+          )}
+        </p>
+        <p className="text-base text-gray-600 ms-1">ريال</p>
+      </div>
+    </div>
+  );
+};
+
+export default InvoiceSummery;
