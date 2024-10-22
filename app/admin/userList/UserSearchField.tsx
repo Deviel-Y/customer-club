7bb8b47e7f4f453ea1055344689f5d9e381b56ff
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-22 21:06:29 +0330

    Add role search field in /userList for S_ADMIN

--- app/admin/userList/UserSearchField.tsx
+++ app/admin/userList/UserSearchField.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { userSearchFieldOnchangeHandlers } from "@/app/utils/onChangeHandlers";
-import { Button, Input } from "@nextui-org/react";
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Button,
+  Input,
+} from "@nextui-org/react";
+import { Role } from "@prisma/client";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const UserSearchField = () => {
@@ -13,6 +19,7 @@ const UserSearchField = () => {
     componyNameOnChangeHandler,
     emailAddressOnChangeHandler,
     itMnagerOnChangeHandler,
+    roleOnChangeHandler,
   } = userSearchFieldOnchangeHandlers(searchParmas, router);
 
   return (
@@ -26,7 +33,7 @@ const UserSearchField = () => {
         تعریف کاربر جدید
       </Button>
 
-      <div className="grid grid-cols-4 grid-rows-1 max-sm:grid-cols-1 max-sm:grid-rows-4 gap-5 max-sm:gap-0 mb-5 w-full">
+      <div className="grid grid-cols-5 grid-rows-1 max-sm:grid-cols-1 max-sm:grid-rows-4 gap-5 max-sm:gap-0 mb-5 w-full">
         <Input
           defaultValue={searchParmas.get("companyName") || ""}
           onChange={componyNameOnChangeHandler}
@@ -58,9 +65,27 @@ const UserSearchField = () => {
           type="search"
           variant="underlined"
         />
+
+        <Autocomplete
+          defaultSelectedKey={searchParmas?.get("role") || "ALL"}
+          listboxProps={{ emptyContent: "نتیجه ای یافت نشد" }}
+          onSelectionChange={roleOnChangeHandler}
+          label="سطح دسترسی"
+          variant="underlined"
+        >
+          {roles.map((role) => (
+            <AutocompleteItem key={role.value}>{role.label}</AutocompleteItem>
+          ))}
+        </Autocomplete>
       </div>
     </div>
   );
 };
 
 export default UserSearchField;
+
+const roles: { label: string; value: Role | "ALL" }[] = [
+  { value: "ALL", label: "همه کاربرها" },
+  { value: "ADMIN", label: "ادمین" },
+  { value: "CUSTOMER", label: "مشتری" },
+];
