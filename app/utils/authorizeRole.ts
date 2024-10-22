87fc79a8a95d27ed1de131bdafb1778b46d63b99
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-22 21:09:59 +0330

    Add new authorization module

--- app/utils/authorizeRole.ts
+++ app/utils/authorizeRole.ts
@@ -6,8 +6,15 @@ export const authorizeUser = (session: Session) => {
 
   if (session?.user?.role !== "CUSTOMER") redirect("/admin");
 };
+
 export const authorizeAdmin = (session: Session) => {
   if (!session) redirect("/api/auth/signin");
 
-  if (session?.user?.role !== "ADMIN") redirect("/");
+  if (session?.user?.role === "CUSTOMER") redirect("/");
+};
+
+export const authorizeSuperAdmin = (session: Session) => {
+  if (!session) redirect("/api/auth/signin");
+
+  if (session?.user?.role !== "SUPER_ADMIN") redirect("/");
 };
