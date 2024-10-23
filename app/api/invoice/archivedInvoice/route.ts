5df1d6ae221dd5cdb5f766b7d914d39c342d56c1
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-23 11:07:19 +0330

    Build POST API to archive invoices

---
+++ app/api/invoice/archivedInvoice/route.ts
@@ -0,0 +1,59 @@
+import {
+  ModifyPorInvoiceType,
+  modifyPorInvoice,
+} from "@/app/libs/validationSchema";
+import prisma from "@/prisma/client";
+import { endOfDay, startOfDay } from "date-fns";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  const body: ModifyPorInvoiceType = await request.json();
+  const { fromDate, toDate } = body;
+
+  const fromDateStart = startOfDay(fromDate);
+  const toDateEnd = endOfDay(toDate);
+
+  const validation = modifyPorInvoice.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json("ورودی داده صحیح نیست", { status: 400 });
+
+  if (fromDate > toDate)
+    return NextResponse.json("تاریخ پایان باید از تاریخ شروع جدید تر باشد", {
+      status: 400,
+    });
+
+  try {
+    const Invoices = await prisma.invoice.findMany({
+      where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
+    });
+    if (Invoices.length === 0)
+      return NextResponse.json("پیش فاکتور در تاریخ ثبت شده یافت نشد", {
+        status: 404,
+      });
+
+    await prisma.$transaction([
+      prisma.archivedInvoice.createMany({
+        data: Invoices.map((invoice) => ({
+          assignedToUserId: invoice?.assignedToUserId,
+          description: invoice?.description,
+          issuerId: invoice?.issuerId,
+          organization: invoice?.organization,
+          organizationBranch: invoice?.organizationBranch,
+          InvoiceNumber: invoice?.invoiceNumber,
+          createdAt: invoice?.createdAt,
+        })),
+      }),
+
+      prisma.invoice.deleteMany({
+        where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
+      }),
+    ]);
+
+    return NextResponse.json("Selected porforma invoices have been archived", {
+      status: 201,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error, { status: 500 });
+  }
+};
