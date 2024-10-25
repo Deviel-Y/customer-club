88e7195d8d43559a92a301acd3eb3580cad7ac75
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-25 20:31:16 +0330

    Create a log record when admin delete some invoice in date rande

--- app/api/invoice/archivedInvoice/Delete-archived-InvoiceRange/route.ts
+++ app/api/invoice/archivedInvoice/Delete-archived-InvoiceRange/route.ts
@@ -1,17 +1,24 @@
+import getSession from "@/app/libs/getSession";
 import {
   ModifyPorInvoiceType,
   modifyPorInvoice,
 } from "@/app/libs/validationSchema";
 import prisma from "@/prisma/client";
 import { endOfDay, startOfDay } from "date-fns";
+import moment from "moment-jalaali";
 import { NextRequest, NextResponse } from "next/server";
 
 export const DELETE = async (request: NextRequest) => {
+  const session = await getSession();
+
   const body: ModifyPorInvoiceType = await request.json();
   const { fromDate, toDate } = body;
 
   const fromDateStart = startOfDay(fromDate);
   const toDateEnd = endOfDay(toDate);
+
+  const jalaliFromDateStart = moment(fromDateStart).format("jYYYY/jM/jD");
+  const jalaliToDateEnd = moment(toDateEnd).format("jYYYY/jM/jD");
 
   const validation = modifyPorInvoice.safeParse(body);
   if (!validation.success)
@@ -23,21 +30,36 @@ export const DELETE = async (request: NextRequest) => {
     });
 
   try {
-    const archivedInvoices = await prisma.archivedInvoice.findMany({
-      where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
-    });
+    const [issuer, archivedInvoices] = await prisma.$transaction([
+      prisma.user.findUnique({
+        where: { id: session?.user.id },
+        select: { adminName: true },
+      }),
+
+      prisma.archivedInvoice.findMany({
+        where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
+        select: { id: true },
+      }),
+    ]);
+
     if (archivedInvoices.length === 0)
       return NextResponse.json("فاکتور در تاریخ ثبت شده یافت نشد", {
         status: 404,
       });
 
-    const archivedInvoiceIds = archivedInvoices.map(
-      (por_invoice) => por_invoice.id
-    );
+    await prisma.$transaction([
+      prisma.archivedInvoice.deleteMany({
+        where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
+      }),
 
-    await prisma.archivedInvoice.deleteMany({
-      where: { id: { in: archivedInvoiceIds } },
-    });
+      prisma.log.create({
+        data: {
+          assignedToSection: "INVOICE",
+          issuer: issuer?.adminName!,
+          message: `کاربر ${issuer?.adminName} فاکتورهای از تاریخ ${jalaliFromDateStart} تا تاریخ ${jalaliToDateEnd} راحذف کرد`,
+        },
+      }),
+    ]);
 
     return NextResponse.json("Selected invoices have been deleted", {
       status: 201,
