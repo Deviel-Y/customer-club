f426c5ae623685edff9b75aef12e3885cbe0815d
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-26 12:15:50 +0330

    Create log record when admin deleted multiple porInvoices

--- app/api/porformaInvoice/deleteMultiple/route.ts
+++ app/api/porformaInvoice/deleteMultiple/route.ts
@@ -1,12 +1,42 @@
+import getSession from "@/app/libs/getSession";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export const DELETE = async (request: NextRequest) => {
-  try {
-    const body = await request.json();
+  const session = await getSession();
 
-    const deletedPorInvoices = await prisma.porformaInvoice.deleteMany({
-      where: { id: { in: body } },
+  try {
+    const body: string[] = await request.json();
+
+    const [deletedPorInvoices, deletedPorInvoiceNumbers, issuer] =
+      await prisma.$transaction([
+        prisma.porformaInvoice.deleteMany({
+          where: { id: { in: body } },
+        }),
+
+        prisma.porformaInvoice.findMany({
+          where: { porformaInvoiceNumber: { in: body } },
+          select: { porformaInvoiceNumber: true },
+        }),
+
+        prisma?.user?.findUnique({
+          where: {
+            id: session?.user.id,
+          },
+          select: { adminName: true },
+        }),
+      ]);
+
+    const deletedPorInvoiceIds = deletedPorInvoiceNumbers
+      .map((porInvoice) => porInvoice.porformaInvoiceNumber)
+      .join(", ");
+
+    await prisma.log.create({
+      data: {
+        assignedToSection: "POR_INVOICE",
+        issuer: issuer?.adminName!,
+        message: `کاربر پیش فاکتور به شماره های ${deletedPorInvoiceIds} را حذف کرد`,
+      },
     });
 
     return NextResponse.json(deletedPorInvoices);
