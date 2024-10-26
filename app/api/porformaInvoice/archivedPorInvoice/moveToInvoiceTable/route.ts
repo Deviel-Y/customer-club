ecaa0c3711404d82610d8a64af58fe23eabac149
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-26 12:35:35 +0330

    Create a log record when admin restored porInvoices in range to its table

--- app/api/porformaInvoice/archivedPorInvoice/moveToInvoiceTable/route.ts
+++ app/api/porformaInvoice/archivedPorInvoice/moveToInvoiceTable/route.ts
@@ -1,17 +1,24 @@
+import getSession from "@/app/libs/getSession";
 import {
   ModifyPorInvoiceType,
   modifyPorInvoice,
 } from "@/app/libs/validationSchema";
 import prisma from "@/prisma/client";
 import { endOfDay, startOfDay } from "date-fns";
+import moment from "moment-jalaali";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
+  const session = await getSession();
+
   const body: ModifyPorInvoiceType = await request.json();
   const { fromDate, toDate } = body;
 
   const fromDateStart = startOfDay(fromDate);
   const toDateEnd = endOfDay(toDate);
+
+  const jalaalifromDateStart = moment(fromDateStart).format("jYYYY/jM/jD");
+  const jalaalitoDateEnd = moment(toDateEnd).format("jYYYY/jM/jD");
 
   const validation = modifyPorInvoice.safeParse(body);
   if (!validation.success)
@@ -23,9 +30,16 @@ export const POST = async (request: NextRequest) => {
     });
 
   try {
-    const porformaInvoices = await prisma.archivedPorformaInvoice.findMany({
-      where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
-    });
+    const [porformaInvoices, issuer] = await prisma.$transaction([
+      prisma.archivedPorformaInvoice.findMany({
+        where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
+      }),
+
+      prisma.user.findUnique({
+        where: { id: session?.user.id },
+        select: { adminName: true },
+      }),
+    ]);
     if (porformaInvoices.length === 0)
       return NextResponse.json("پیش فاکتور در تاریخ ثبت شده یافت نشد", {
         status: 404,
@@ -48,6 +62,14 @@ export const POST = async (request: NextRequest) => {
 
       prisma.archivedPorformaInvoice.deleteMany({
         where: { createdAt: { lte: toDateEnd, gte: fromDateStart } },
+      }),
+
+      prisma.log.create({
+        data: {
+          assignedToSection: "POR_INVOICE",
+          issuer: issuer?.adminName!,
+          message: `کاربر ${issuer?.adminName} پیش فاکتورهای از تاریخ ${jalaalifromDateStart} تا تاریخ ${jalaalitoDateEnd} را به پیش فاکتورها منتقل کرد`,
+        },
       }),
     ]);
 
