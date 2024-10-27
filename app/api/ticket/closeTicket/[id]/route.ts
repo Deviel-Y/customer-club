c58c8eafff54a87339a70318136f7ff2f91042e6
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-27 18:05:15 +0330

    Build PATCH API to close a spicific ticket

---
+++ app/api/ticket/closeTicket/[id]/route.ts
@@ -0,0 +1,48 @@
+import getSession from "@/app/libs/getSession";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+  params: { id: string };
+}
+
+export const PATCH = async (
+  request: NextRequest,
+  { params: { id } }: Props
+) => {
+  const session = await getSession();
+
+  const body = await request.json();
+  const { ticketId } = body;
+
+  const ticket = await prisma.ticket.findUnique({
+    where: { id },
+    select: { ticketNumber: true },
+  });
+  if (!ticket) return NextResponse.json("Ticket not found", { status: 404 });
+
+  const [issuer, updatedTicket] = await prisma.$transaction([
+    prisma.user.findUnique({
+      where: { id: session?.user.id },
+      select: { adminName: true },
+    }),
+
+    prisma.ticket.update({
+      where: { id: ticketId },
+      data: { status: "CLOSED" },
+      select: { ticketNumber: true },
+    }),
+  ]);
+
+  await prisma.log.create({
+    data: {
+      assignedToSection: "TICKET",
+      issuer: issuer?.adminName!,
+      message: `تیکت شماره ${updatedTicket.ticketNumber} توسط ${issuer?.adminName} بسته شد`,
+    },
+  });
+
+  return NextResponse.json(
+    `Ticket ${ticket.ticketNumber} has been closed by ${issuer?.adminName}`
+  );
+};
