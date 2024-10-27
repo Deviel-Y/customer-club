65c0f799dc220701296c050164224b96feaabcba
Daniel Yadghar <daniel072.dy@gmail.com>
2024-10-27 19:39:48 +0330

    Apply RWD to /admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -49,7 +49,7 @@ const AdminPage = async () => {
   ];
 
   return (
-    <div className="grid grid-cols-4 max-md:grid-cols-3 grid-rows-1 max-sm:grid-rows-3 max-sm:grid-cols-1 gap-5 p-5">
+    <div className="grid grid-cols-4 max-md:grid-cols-2 grid-rows-1 max-sm:grid-rows-3 max-sm:grid-cols-1 gap-5 p-5">
       {dashboardCardInfo.map((info) => (
         <DashboardCard
           amount={info.amount}
